144e72380a89e336536a44c1893940f56a8532f7
Konaya245 <mzaidi.ar@gmail.com>
2024-01-29 18:27:46 +0800

    Revert "Revert "Merge branch 'qoys' into zaidi""
    This reverts commit cd85c35223d77f8f845a409fa450fe2f015241c4.

--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -14,52 +14,52 @@ class ProfilePage extends StatelessWidget {
         backgroundColor: Colors.purple[300],
         title: Text('Profile Page'),
       ),
-      body:  Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
               padding: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
-              border: Border.all(color: Colors.blueAccent),
-              borderRadius: BorderRadius.circular(20.0),
+                border: Border.all(color: Colors.blueAccent),
+                borderRadius: BorderRadius.circular(20.0),
               ),
               child: Text(
                 'Username:  ${user?.displayName ?? ''}',
-                    style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold, // Make the text bold
-                  ),
-                ),
-              ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-              border: Border.all(color: Colors.blueAccent),
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-              child: Text(
-                'Email:  ${user?.email?? ''}',
-                  style: TextStyle(
+                style: TextStyle(
                   fontSize: 20.0,
                   fontWeight: FontWeight.bold, // Make the text bold
                 ),
               ),
             ),
-              Container(  
-                padding: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
+            Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
                 border: Border.all(color: Colors.blueAccent),
                 borderRadius: BorderRadius.circular(20.0),
               ),
-                child: Text(
-                  'Kulliyyah:  ${user?.displayName ?? ''}',
-                    style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold, // Make the text bold
-                  ),
+              child: Text(
+                'Email:  ${user?.email ?? ''}',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold, // Make the text bold
                 ),
               ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueAccent),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Text(
+                'Kulliyyah:  ${user?.displayName ?? ''}',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold, // Make the text bold
+                ),
+              ),
+            ),
           ],
         ),
       ),
