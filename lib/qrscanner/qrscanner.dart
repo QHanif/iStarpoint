39fea392c4e308d91756c8c6990518ee892e95a1
Hanif <qoysjaafar@gmail.com>
2024-01-29 23:28:34 +0800

    qr starpoint

--- lib/qrscanner/qrscanner.dart
+++ lib/qrscanner/qrscanner.dart
@@ -1,10 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:iStarpoint/qrscanner/qrscanner.dart';
+// import 'package:iStarpoint/qrscanner/qrscanner.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
-
-void main () {
+void main() {
   runApp(const MyApp());
 }
 
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'QR Scanner',
-      home: const QrScanner(),
+      home: QrScanner(),
     );
   }
 }
@@ -28,32 +28,77 @@ class QrScanner extends StatefulWidget {
 }
 
 class _QrScannerState extends State<QrScanner> {
-  String qrResult='Scanned Data will appear here';
-
+  String qrResult = 'Scanned Data will appear here';
   Future<void> scanQR() async {
-    try{
-      final qrCode = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.QR);
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+
       if (!mounted) return;
       setState(() {
         this.qrResult = qrCode.toString();
       });
 
-    }on PlatformException{
-      qrResult = 'Failed to read QR Code';}
+      // Split the QR code data into a list of strings
+      List<String> qrData = qrResult.split(',');
+
+      // Extract the voucher name, voucher value, and email from the list
+      String voucherName = qrData[0].trim();
+      String voucherValue = qrData[1].trim();
+      String email = qrData[2].trim();
+      int voucherValueInt = int.parse(voucherValue.trim());
+
+      // Get a reference to the document
+      // DocumentReference docRef =
+      //     FirebaseFirestore.instance.collection('Users').doc(email);
+
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      if (email != null) {
+        // Query the Users collection for the user document
+        QuerySnapshot userQuery = await firestore
+            .collection('Users')
+            .where('Email', isEqualTo: email)
+            .get();
+
+        // Get the user's document
+        DocumentSnapshot userDoc = userQuery.docs.first;
+
+        // Increase the user's starpoints by 30
+        int newStarpoints = userDoc['Starpoints'] - voucherValueInt;
+
+        // Update the user's document
+        await firestore
+            .collection('Users')
+            .doc(userDoc.id)
+            .update({'Starpoints': newStarpoints});
+      }
+
+      // Update the document
+      // await docRef.update({
+      //   'starpoint': FieldValue.increment(-int.parse(voucherValue)),
+      // });
+    } on PlatformException {
+      qrResult = 'Failed to read QR Code';
+    } catch (e) {
+      print('Error updating document: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('QR Scanner')),
+      appBar: AppBar(title: const Text('QR Scanner')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-              SizedBox(height: 30),
-              Text('$qrResult',style:TextStyle(color:Colors.black),),
-              SizedBox(height: 30),
-              ElevatedButton(onPressed: scanQR, child: Text('Scan QR Code'))
+            const SizedBox(height: 30),
+            Text(
+              qrResult,
+              style: const TextStyle(color: Colors.black),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(onPressed: scanQR, child: const Text('Scan QR Code'))
           ],
         ),
       ),
